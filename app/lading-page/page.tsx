c5860b7f8abec8754c5d3682c8a8f9ec618c0017
Ryan Figueredo <ryan.figueredo059@gmail.com>
2024-11-22 16:45:34 -0300

    feat: Add new SVG assets and enhance landing page layout with images and improved content

--- app/lading-page/page.tsx
+++ app/lading-page/page.tsx
@@ -1,26 +1,84 @@
 import React from "react";
-import { ArrowUpRight } from "lucide-react";
+import { ArrowLeft, ArrowRight, ArrowUpRight } from "lucide-react";
 import { Button } from "../_components/ui/button";
 import Header from "./components/header";
+import Image from "next/image";
 
 const LandingPage: React.FC = () => {
   return (
     <div className="h-screen w-full bg-[#F8F8F8] p-6">
       <Header />
-      <h1 className="text-4xl font-bold text-[#353232]">
-        Sua Solução <br />
-        Digital <br />
-        Começa Aqui
-      </h1>
-      <p>
-        Consultoria gratuita para <br />
-        tirar sua ideia do papel com <br />
-        tecnologia ágil e eficiente.
-      </p>
+      <div className="flex flex-col items-center justify-between gap-8 p-8 md:flex-row md:items-center">
+        <div className="flex-1 space-y-6">
+          <h1 className="text-4xl font-bold text-[#353232] md:text-5xl">
+            Sua Solução <br />
+            Digital <br />
+            Começa Aqui
+          </h1>
+          <p className="text-justify text-base text-[#353232] md:w-56 md:text-lg">
+            Consultoria gratuita para tirar sua ideia do papel com tecnologia
+            ágil e eficiente.
+          </p>
+          <Button>
+            Iniciar meu projeto <ArrowUpRight />
+          </Button>
+        </div>
 
-      <Button>
-        Iniciar meu projeto <ArrowUpRight />
-      </Button>
+        <div className="flex flex-1 justify-center">
+          <Image
+            src="hero.svg"
+            alt="Ilustração de lâmpadas representando ideias"
+            className="h-auto w-auto max-w-xs md:max-w-md"
+            height={473}
+            width={474}
+            priority={true}
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center gap-8 py-64">
+        <div className="flex flex-wrap justify-center gap-8">
+          <Image src="kl.svg" height={40} width={80} alt="" />
+          <Image src="barber.svg" height={40} width={80} alt="" />
+          <Image src="mury.svg" height={40} width={80} alt="" />
+          <Image src="netshoes.svg" height={40} width={80} alt="" />
+          <Image src="pedracom.svg" height={40} width={80} alt="" />
+          <Image src="tz.svg" height={40} width={80} alt="" />
+        </div>
+
+        <div className="space-y-4 text-center">
+          <h2 className="text-lg text-[#353232]">
+            Mais de 50 projetos realizados com sucesso para empresas de diversos
+            setores. <br />
+            Junte-se a nós e transforme seu negócio em um case de sucesso.
+          </h2>
+          <Button>
+            Marcar uma conversa <ArrowRight />
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center gap-8 pb-64">
+        <div className="flex flex-wrap justify-center gap-8">
+          <Image src="kl.svg" height={40} width={80} alt="" />
+          <Image src="barber.svg" height={40} width={80} alt="" />
+          <Image src="mury.svg" height={40} width={80} alt="" />
+          <Image src="netshoes.svg" height={40} width={80} alt="" />
+          <Image src="pedracom.svg" height={40} width={80} alt="" />
+          <Image src="tz.svg" height={40} width={80} alt="" />
+        </div>
+
+        <div className="space-y-4 text-center">
+          <h2 className="text-lg text-[#353232]">
+            Mais de 50 projetos realizados com sucesso para empresas de diversos
+            setores. <br />
+            Junte-se a nós e transforme seu negócio em um case de sucesso.
+          </h2>
+          <Button>
+            Marcar uma conversa <ArrowRight />
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
